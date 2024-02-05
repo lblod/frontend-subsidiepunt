77eed856826cc2a497a86eb60f10a86e0ef34159
wolfez <wolfderechter@gmail.com>
2024-02-05 13:12:50 +0100

    redirect to subsidy index page after login

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,10 @@ import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
   @service session;
+  @service router;
 
   async beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+    this.router.transitionTo('subsidy.applications');
   }
 }
