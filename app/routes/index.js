e032b47e66b2e46b73c8b6258ba1aa91140cf768
Achraf Atauil <52280338+aatauil@users.noreply.github.com>
2024-03-19 13:15:12 +0100

    Revert "redirect to subsidy index page after login"
    This reverts commit 8169e65395adf2698c5911d2e29db8cdae1b2cc9.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,8 @@ import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
   @service session;
-  @service router;
 
   async beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
-    this.router.transitionTo('subsidy.applications');
   }
 }
