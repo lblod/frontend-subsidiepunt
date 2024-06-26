57343ecc9aabe8ec261c1c74d2d6725a8d6c118f
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-06-26 11:42:11 +0200

    Remove `rdfaBindings` properties
    This was only used by the `ember-rdfa-helpers` library, which isn't used by this app.

--- app/models/bestuurseenheid.js
+++ app/models/bestuurseenheid.js
@@ -14,12 +14,4 @@ export default class Bestuurseenheid extends Model {
     inverse: null,
   })
   classificatie;
-
-  rdfaBindings = {
-    naam: 'http://www.w3.org/2004/02/skos/core#prefLabel',
-    class: 'http://data.vlaanderen.be/ns/besluit#Bestuurseenheid',
-    werkingsgebied: 'http://data.vlaanderen.be/ns/besluit#werkingsgebied',
-    bestuursorgaan: 'http://data.vlaanderen.be/ns/besluit#bestuurt',
-    classificatie: 'http://data.vlaanderen.be/ns/besluit#classificatie',
-  };
 }
