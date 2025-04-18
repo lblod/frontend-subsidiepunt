aac27b45ef5b73b3baf936773aa374360e6f1351
Achraf <achrafatauil@hotmail.com>
2025-04-18 13:14:20 +0200

    Requires authentication for route access

--- app/routes/geen-toegang.js
+++ app/routes/geen-toegang.js
@@ -4,8 +4,10 @@ import { inject as service } from '@ember/service';
 export default class GeenToegangRoute extends Route {
   @service currentSession;
   @service router;
+  @service session;
 
-  beforeModel() {
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
     const hasSubsidiepuntGebruikerRole = this.currentSession.roles.includes("SubsidiepuntGebruiker");
 
     if (hasSubsidiepuntGebruikerRole) {
