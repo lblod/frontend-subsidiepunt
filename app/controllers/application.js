fe943f43b7b379967ba67f5647071a7aec16e279
Achraf <achrafatauil@hotmail.com>
2025-05-14 14:44:00 +0200

    Enhances user info display in header
    Improves the application header to display more comprehensive user information, including user name, group, and classification. This change also introduces an impersonation menu for admins to switch user accounts, providing a better user experience and improved administrative capabilities.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,9 +2,10 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
-  @service() session;
-  @service() currentSession;
-  @service() router;
+  @service session;
+  @service currentSession;
+  @service router;
+  @service impersonation;
 
   appTitle = 'SubsidiePunt';
 
@@ -14,5 +15,44 @@ export default class ApplicationController extends Controller {
       this.router.currentRouteName === 'geen-toegang' ||
       this.router.currentRouteName === 'subsidy.applications.index_loading'
     );
+  }
+
+  get userInfo() {
+    let user;
+    let group;
+    let classification;
+
+    if (this.impersonation.isImpersonating) {
+      user = this.impersonation.originalAccount.gebruiker;
+      group = this.impersonation.originalGroup;
+      classification = group.belongsTo('classificatie').value();
+    } else {
+      user = this.currentSession.user;
+      group = this.currentSession.group;
+      classification = this.currentSession.groupClassification;
+    }
+
+    if (!user) {
+      return '';
+    }
+
+    let userInfo = user.fullName;
+    let groupInfo = '';
+
+    if (classification?.label) {
+      groupInfo += classification.label;
+    }
+
+    if (group?.naam) {
+      groupInfo += ` ${group.naam}`;
+    }
+
+    groupInfo.trim();
+
+    if (groupInfo.length) {
+      userInfo += ` - ${groupInfo}`;
+    }
+
+    return userInfo;
   }
 }
