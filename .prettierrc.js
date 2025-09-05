a6c2643667d94a0e2ac45bfa7928769e016d1c33
Achraf <achrafatauil@hotmail.com>
2025-09-05 12:37:53 +0200

    update config files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,13 @@
 'use strict';
 
 module.exports = {
+  plugins: ['prettier-plugin-ember-template-tag'],
   overrides: [
     {
       files: '*.{js,mjs,ts,gjs,gts}',
       options: {
         singleQuote: true,
+        templateSingleQuote: false,
       },
     },
   ],
