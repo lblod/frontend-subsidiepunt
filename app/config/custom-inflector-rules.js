2b6baf6f123ce2d2e1ae06316f33d2e5cca65aa0
Achraf <achrafatauil@hotmail.com>
2025-09-05 12:38:41 +0200

    replace ember-inflector package

--- app/config/custom-inflector-rules.js
+++ app/config/custom-inflector-rules.js
@@ -1,54 +1,53 @@
-import Inflector from 'ember-inflector';
+import { irregular, plural, singular } from '@ember-data/request-utils/string';
 
-const inflector = Inflector.inflector;
+irregular('bestuurseenheid', 'bestuurseenheden');
+plural(/$/, 'en');
+plural(/e$/, 'es');
+plural(/e([lnr])$/, 'e$1s');
+plural(/([aiuo])$/, '$1s');
+plural(/([^aiuoe])([aiuo])([a-z])$/, '$1$2$3$3en'); // TODO: this is a bit hack
+plural(/uis$/, 'uizen');
+plural(/ief$/, 'ieven');
+plural(/or$/, 'oren');
+plural(/ie$/, 'ies');
+plural(/eid$/, 'eden');
+plural(/aa([a-z])$/, 'a$1en');
+plural(/uu([a-z])$/, 'u$1en');
+plural(/oo([a-z])$/, 'o$1en');
+singular(/en$/, '');
+singular(/es$/, 'e');
+singular(/e([lnr])s$/, 'e$1');
+singular(/([aiuo])s$/, '$1');
+singular(/([^aiuoe])([aiuo])([a-z])\3en$/, '$1$2$3'); // TODO: this is a bit hack
+singular(/uizen$/, 'uis');
+singular(/ieven$/, 'ief');
+singular(/ies$/, 'ie');
+singular(/eden$/, 'eid');
+singular(/a([a-z])en$/, 'aa$1');
+singular(/u([a-z])en$/, 'uu$1');
+singular(/o([a-z])en$/, 'oo$1');
+singular(/([auio])s$/, '$1s');
 
-inflector.plural(/$/, 'en');
-inflector.plural(/e$/, 'es');
-inflector.plural(/e([lnr])$/, 'e$1s');
-inflector.plural(/([aiuo])$/, '$1s');
-inflector.plural(/([^aiuoe])([aiuo])([a-z])$/, '$1$2$3$3en'); // TODO: this is a bit hack
-inflector.plural(/uis$/, 'uizen');
-inflector.plural(/ief$/, 'ieven');
-inflector.plural(/or$/, 'oren');
-inflector.plural(/ie$/, 'ies');
-inflector.plural(/eid$/, 'eden');
-inflector.plural(/aa([a-z])$/, 'a$1en');
-inflector.plural(/uu([a-z])$/, 'u$1en');
-inflector.plural(/oo([a-z])$/, 'o$1en');
-inflector.singular(/en$/, '');
-inflector.singular(/es$/, 'e');
-inflector.singular(/e([lnr])s$/, 'e$1');
-inflector.singular(/([aiuo])s$/, '$1');
-inflector.singular(/([^aiuoe])([aiuo])([a-z])\3en$/, '$1$2$3'); // TODO: this is a bit hack
-inflector.singular(/uizen$/, 'uis');
-inflector.singular(/ieven$/, 'ief');
-inflector.singular(/ies$/, 'ie');
-inflector.singular(/eden$/, 'eid');
-inflector.singular(/a([a-z])en$/, 'aa$1');
-inflector.singular(/u([a-z])en$/, 'uu$1');
-inflector.singular(/o([a-z])en$/, 'oo$1');
-inflector.singular(/([auio])s$/, '$1s');
-
-inflector.irregular('account', 'accounts');
-inflector.irregular('file', 'files');
-inflector.irregular('remote-url', 'remote-urls');
-inflector.irregular('subsidy-application-form', 'subsidy-application-forms');
-inflector.irregular(
+irregular('account', 'accounts');
+irregular('file', 'files');
+irregular('remote-url', 'remote-urls');
+irregular('subsidy-application-form', 'subsidy-application-forms');
+irregular(
   'submission-document-status',
   'submission-document-statuses',
 );
-inflector.irregular('participation', 'participations');
-inflector.irregular('organization', 'organizations');
-inflector.irregular('period-of-time', 'periods-of-time');
-inflector.irregular(
+irregular('participation', 'participations');
+irregular('organization', 'organizations');
+irregular('period-of-time', 'periods-of-time');
+irregular(
   'subsidy-measure-consumption',
   'subsidy-measure-consumptions',
 );
-inflector.irregular(
+irregular(
   'subsidy-application-flow-step',
   'subsidy-application-flow-steps',
 );
-inflector.irregular(
+irregular(
   'subsidy-measure-consumption-status',
   'subsidy-measure-consumption-statuses',
 );
